4c560b23ac0097a06cc69bcd3cd837fb17db16b5
Vikas2426 <vikas.cby@gmail.com>
2024-04-13 20:19:25 +0530

    fix name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          VIkas Choubey
+          Vikas Choubey
         </a>
       </header>
     </div>
